d8f3554ba56fc78d32f81cb0fdb309ef22c4035c
Dasara Sai Ram <126001270+sairamdasara@users.noreply.github.com>
2026-03-28 09:27:50 +0530

    Create Role.js

---
+++ Role.js
@@ -0,0 +1,7 @@
+module.exports = function (roles) {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role))
+      return res.status(403).send("Access Forbidden");
+    next();
+  };
+};
